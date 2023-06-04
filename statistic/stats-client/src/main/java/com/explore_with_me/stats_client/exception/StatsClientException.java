73a5a7ed1c0f1f193cb27f1fb98278dd484530d7
Eugene <pokhily@yandex.ru>
2023-06-04 23:42:58 +0300

    Diploma: stat server (part 1)

--- statistic/stats-client/src/main/java/com/explore_with_me/stats_client/exception/StatsClientException.java
+++ statistic/stats-client/src/main/java/com/explore_with_me/stats_client/exception/StatsClientException.java
@@ -1,6 +1,6 @@
 package com.explore_with_me.stats_client.exception;
 
-public class StatsClientException extends RuntimeException{
+public class StatsClientException extends RuntimeException {
     public StatsClientException(String message) {
         super(message);
     }
