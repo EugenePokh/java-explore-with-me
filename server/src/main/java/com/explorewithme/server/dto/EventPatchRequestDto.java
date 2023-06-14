5a265d61e3a13ad8784f16e85dff2a675e7e1447
Eugene <pokhily@yandex.ru>
2023-06-14 22:16:14 +0300

    Diploma: main server (part 2)

--- server/src/main/java/com/explorewithme/server/dto/EventPatchRequestDto.java
+++ server/src/main/java/com/explorewithme/server/dto/EventPatchRequestDto.java
@@ -6,7 +6,6 @@ import lombok.Data;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PositiveOrZero;
 import java.time.LocalDateTime;
 
